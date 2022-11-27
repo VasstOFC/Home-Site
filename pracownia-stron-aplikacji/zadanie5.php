99d76fcd2aa92c64d05321bd07039e149732085b
dfraczek1 <vastofc@gmail.com>
2022-11-27 18:20:23 +0100

    update

---
+++ pracownia-stron-aplikacji/zadanie5.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="https://i.imgur.com/5AhVa3e.png" type="image/x-icon">
+    <title>Ćwiczenie 6</title>
+    <style>
+        body {
+            background-color: darkgrey;
+        }
+        div {
+            width: 400px;
+            background-color: lightgray;
+            padding: 10px;
+            border-radius: 15px;
+            box-shadow: 5px 10px 5px lightcyan;
+        }
+
+        p {
+            font-size: 14pt;
+        }
+
+        .pole {
+            position: absolute;
+            left: 130px;
+        }
+
+        #button {
+            padding: 10px;
+            background-color: white;
+            border-radius: 5px;
+            margin-top: 15px;
+            border: 0px;
+        }
+
+        #button:hover {
+            -webkit-transition: all 0.5s ease;
+            -moz-transition: all 0.5s ease;
+            -ms-transition: all 0.5s ease;
+            -o-transition: all 0.5s ease;
+            transition: all 0.5s ease;
+            background-color: darkgoldenrod;
+        }
+
+        input {
+            border: 0;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+
+<body>
+    <div>
+        <form action="index.php" method="POST">
+
+            <b>Numer PESEL:</b>
+            <input type="text" class="pole" name="pesel"><br>
+            <input type="submit" value="Wyślij" id="button">
+        </form>
+
+        <?php
+        function walidacja()
+        {
+            $pesel = $_POST['pesel'];
+            echo '<br><hr>';
+
+            /*           Sprawdzanie ilości znaków             */
+            if (preg_match('/^[0-9]{11}$/', $pesel)) {
+            } else {
+                echo '<p style="margin: 0; color: red">PESEL nie ma 11 znaków</p>';
+            }
+
+            /*     Sprawdzanie czy podana wartość to cygry     */
+            if (ctype_digit($pesel) === true) {
+            } else {
+                echo '<p style="margin: 0; color: red">PESEL musi składać się tylko z cyfr!</p>';
+            }
+
+            /*           Sprawdzanie sumy kontrolnej           */
+            $wagi = array(1, 3, 7, 9, 1, 3, 7, 9, 1, 3);
+            $walidacja = 0;
+            for ($i = 0; $i < 10; $i++) {
+                $walidacja += $wagi[$i] * !empty($pesel[$i]);
+            }
+            $odejmowanie = 10 - $walidacja % 10;
+            $liczba_kontrolna = ($odejmowanie == 10) ? 0 : $odejmowanie;
+            if ($liczba_kontrolna == !empty($pesel[10])) {
+                echo '<p style="margin: 0; color: green">Suma kontrolna jest poprawna!</p>';
+            } else {
+                echo '<p style="margin: 0; color: red">Suma kontrolna jest niepoprawna!</p>';
+            }
+            echo '<hr>';
+
+            /*                 Sprawdzanie płci                */
+            if ($pesel[9] % 2 == 1) {
+                echo 'Płeć: Mężczyzna';
+            } else {
+                echo 'Płeć: Kobieta';
+            }
+
+        }
+        walidacja();
+        ?>
+
+</body>
+
+</html>
--- pracownia-stron-aplikacji/zadanie6.php
+++ pracownia-stron-aplikacji/zadanie6.php
@@ -3,106 +3,36 @@
 
 <head>
     <meta charset="UTF-8">
-    <link rel="shortcut icon" href="https://i.imgur.com/5AhVa3e.png" type="image/x-icon">
-    <title>Ćwiczenie 6</title>
-    <style>
-        body {
-            background-color: darkgrey;
-        }
-        div {
-            width: 400px;
-            background-color: lightgray;
-            padding: 10px;
-            border-radius: 15px;
-            box-shadow: 5px 10px 5px lightcyan;
-        }
-
-        p {
-            font-size: 14pt;
-        }
-
-        .pole {
-            position: absolute;
-            left: 130px;
-        }
-
-        #button {
-            padding: 10px;
-            background-color: white;
-            border-radius: 5px;
-            margin-top: 15px;
-            border: 0px;
-        }
-
-        #button:hover {
-            -webkit-transition: all 0.5s ease;
-            -moz-transition: all 0.5s ease;
-            -ms-transition: all 0.5s ease;
-            -o-transition: all 0.5s ease;
-            transition: all 0.5s ease;
-            background-color: darkgoldenrod;
-        }
-
-        input {
-            border: 0;
-            border-radius: 5px;
-        }
-    </style>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zadanie 6</title>
 </head>
 
 <body>
-    <div>
-        <form action="index.php" method="POST">
+    <?php
+    /* 
+    skrypt wysyła do bazy danych zapytanie wybierające jedynie pola id, nazwa, opis, cena z tabeli podzespoly dla tych podzespołów, których cena jest niższa niż 1000 zł 
+    każdy zwrócony zapytaniem wiersz jest wyświetlany w osobnym wierszu tabeli w panelu głównym zgodnie z obrazem 2 
+    */
+    $polaczenie = mysqli_connect('localhost', 'root', '', 'sklep');
+    $zapytanie1 = "SELECT id, nazwa , opis, cena FROM podzespoly WHERE cena < 1000";
+    $wynik = mysqli_query ($polaczenie, $zapytanie1);
+    echo "<table>";
+    echo "<tr>"."<th>NUMER</th>"."<th>NAZWA PODZESPOŁU</th>"."<th>OPIS</th>"."<th>CENA</th>"."</tr>";
+    while ($wiersz = mysqli_fetch_array($wynik)) {
+        echo "<tr><td>"."$wiersz[id]"."</td>";
+        echo "<td>" ."$wiersz[nazwa]"."</td>";
+        echo "<td>" ."$wiersz[opis]"."</td>";
+        echo "<td>" ."$wiersz[cena]"."</td></tr>";
+    }
+    echo "</table>";
 
-            <b>Numer PESEL:</b>
-            <input type="text" class="pole" name="pesel"><br>
-            <input type="submit" value="Wyślij" id="button">
-        </form>
 
-        <?php
-        function walidacja()
-        {
-            $pesel = $_POST['pesel'];
-            echo '<br><hr>';
 
-            /*           Sprawdzanie ilości znaków             */
-            if (preg_match('/^[0-9]{11}$/', $pesel)) {
-            } else {
-                echo '<p style="margin: 0; color: red">PESEL nie ma 11 znaków</p>';
-            }
+    mysqli_close($polaczenie);
+    ?>
 
-            /*     Sprawdzanie czy podana wartość to cygry     */
-            if (ctype_digit($pesel) === true) {
-            } else {
-                echo '<p style="margin: 0; color: red">PESEL musi składać się tylko z cyfr!</p>';
-            }
-
-            /*           Sprawdzanie sumy kontrolnej           */
-            $wagi = array(1, 3, 7, 9, 1, 3, 7, 9, 1, 3);
-            $walidacja = 0;
-            for ($i = 0; $i < 10; $i++) {
-                $walidacja += $wagi[$i] * !empty($pesel[$i]);
-            }
-            $odejmowanie = 10 - $walidacja % 10;
-            $liczba_kontrolna = ($odejmowanie == 10) ? 0 : $odejmowanie;
-            if ($liczba_kontrolna == !empty($pesel[10])) {
-                echo '<p style="margin: 0; color: green">Suma kontrolna jest poprawna!</p>';
-            } else {
-                echo '<p style="margin: 0; color: red">Suma kontrolna jest niepoprawna!</p>';
-            }
-            echo '<hr>';
-
-            /*                 Sprawdzanie płci                */
-            if ($pesel[9] % 2 == 1) {
-                echo 'Płeć: Mężczyzna';
-            } else {
-                echo 'Płeć: Kobieta';
-            }
-
-        }
-        walidacja();
-        ?>
-
+</table>
 </body>
 
 </html>
